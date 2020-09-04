d8d38c0f08724ce976d9442d6a648a011afe3088
MD. Zariful Huq <52840169+zarif007@users.noreply.github.com>
2020-09-05 02:50:54 +0600

    Update MainApp.py

--- App/MainApp.py
+++ App/MainApp.py
@@ -8,6 +8,12 @@ from kivy.uix.image import Image
 from kivy.lang import Builder,BuilderException
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.uix.list import OneLineListItem
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 
 link_helper="""
@@ -15,10 +21,35 @@ MDTextField:
     hint_text : "Enter URL"
     helper_text : "Please Enter URL"
     icon_right:"link"
-    pos_hint :{'center_x':0.5 , 'center_y':0.4}
+    pos_hint :{'center_x':0.5 , 'center_y':0.45}
     size_hint_x:None
-    width:300
+    width:500
 """
+
+link1_helper="""
+MDTextField:
+    hint_text : "Enter Image/Image URL"
+    helper_text : "Please Enter URL"
+    icon_right:"image"
+    pos_hint :{'center_x':0.5 , 'center_y':0.32}
+    size_hint_x:None
+    width:500
+"""
+
+link2_helper="""
+MDTextField:
+    hint_text : "Enter Text"
+    helper_text : "Please Enter Text"
+    icon_right:"text"
+    pos_hint :{'center_x':0.5 , 'center_y':0.21}
+    size_hint_x:None
+    width:500
+"""
+
+
+
+
+
 nav_helper="""
 Screen:
     NavigationLayout:
@@ -100,13 +131,21 @@ class NSA(MDApp):
         screen=Screen()
         hlp=Builder.load_string(nav_helper)
         #label=MDLabel(text='News Source Authenticator' ,  halign='center', theme_text_color='Error' , font_style='H6')
-        button=MDRectangleFlatButton(text='Check', pos_hint ={'center_x':0.5 , 'center_y':0.3} , on_release=self.check_source)
+        button=MDRectangleFlatButton(text='Check', pos_hint ={'center_x':0.5 , 'center_y':0.39} , on_release=self.check_source)
+        buttonl1=MDRectangleFlatButton(text='Check', pos_hint ={'center_x':0.5 , 'center_y':0.26} , on_release=self.check_image)
+        buttonl2=MDRectangleFlatButton(text='Check', pos_hint ={'center_x':0.5 , 'center_y':0.15} , on_release=self.check_text)
         button1=MDRoundFlatButton(text='Change Mode', pos_hint ={'center_x':0.8 , 'center_y':0.95} , on_release=self.change_colour)
         img = Image(source='Add a heading (1).png')
         self.link=Builder.load_string(link_helper)
+        self.link1=Builder.load_string(link1_helper)
+        self.link2=Builder.load_string(link2_helper)
         screen.add_widget(self.link)
+        screen.add_widget(self.link1)
+        screen.add_widget(self.link2)
         screen.add_widget(button)
         screen.add_widget(button1)
+        screen.add_widget(buttonl1)
+        screen.add_widget(buttonl2)
         screen.add_widget(img)
         screen.add_widget(hlp)
         return screen
@@ -119,6 +158,8 @@ class NSA(MDApp):
         else:
             self.theme_cls.theme_style="Light"
             
+    fakeList=[]
+    fc=0
 
     def check_source(self,obj):
         lis=['https://www.atnbangla.tv/','https://www.dhakatribune.com/','https://www.prothomalo.com/','https://bangla.dhakatribune.com/' ,
@@ -140,7 +181,8 @@ class NSA(MDApp):
         'https://www.bbc.com/bengali/','https://www.dw.com/bn/','https://www3.nhk.or.jp/nhkworld/bn/','https://parstoday.com/bn','https://www.voabangla.com/',
         'https://www.thedailystar.net/','https://www.newagebd.net/','https://www.daily-sun.com/','https://thebangladeshtoday.com/','https://thefinancialexpress.com.bd/',]
 
-
+    
+    
         s=str(self.link.text)
         z=len(s)
         f=""
@@ -170,6 +212,9 @@ class NSA(MDApp):
             self.dialog=MDDialog(title='REAL NEWS' , text="The news source "+f[8:ln]+"  is legit and there is high chance of being this news true.",size_hint=(0.7,2),buttons=[close_button])
             self.dialog.open()
         else:
+            self.fakeList.append(f[8:ln])
+            print(self.fakeList[self.fc])
+            self.fc=+1
             self.dialog=MDDialog(title='FAKE NEWS' ,  text="The news source "+f[8:ln]+"  is not legit and there is high chance of being this news fake.",size_hint=(0.7,2),buttons=[close_button])
             self.dialog.open()
 
@@ -177,6 +222,30 @@ class NSA(MDApp):
         self.dialog.dismiss()
 
 
-NSA().run()
+    def check_text(self,obj):
+        s=self.link2.text 
+        options = webdriver.ChromeOptions() 
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        driver = webdriver.Chrome(options=options, executable_path=r'F:\chromedriver.exe')
+        driver.get("https://google.com/")
 
+        search = driver.find_element_by_name("q")
+        search.send_keys(s)
+        search.send_keys(Keys.RETURN)
+        time.sleep(10)
+
+    def check_image(self,obj):
+        s=self.link1.text 
+        options = webdriver.ChromeOptions() 
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        driver = webdriver.Chrome(options=options, executable_path=r'F:\chromedriver.exe')
+        driver.get("https://google.com/")
+
+        search = driver.find_element_by_name("q")
+        search.send_keys(s)
+        search.send_keys(Keys.RETURN)
+        time.sleep(10)
+
+
+NSA().run()
 
