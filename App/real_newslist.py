b29e49da9da9e5ccb8867f51ae5e60925d48ffab
MD. Zariful Huq <52840169+zarif007@users.noreply.github.com>
2020-11-28 16:29:16 +0600

    Update real_newslist.py

--- App/real_newslist.py
+++ App/real_newslist.py
@@ -1,5 +1,6 @@
 lis=['https://www.atnbangla.tv/','https://www.dhakatribune.com/','https://www.prothomalo.com/','https://bangla.dhakatribune.com/' ,
-        'https://samakal.com/','https://www.jugantor.com/' 'https://www.dailyinqilab.com/''https://www.dailynayadiganta.com/','https://www.kalerkantho.com/',
+        'https://samakal.com/','https://www.jugantor.com/' 'https://www.dailyinqilab.com/',
+        'https://www.dailynayadiganta.com/','https://www.kalerkantho.com/',
         'https://www.bhorerkagoj.com/','https://www.bangla.24livenewspaper.com/',
         'https://www.dailyjanakantha.com/','https://www.24livenewspaper.com/',
         'https://www.bd-journal.com/','https://www.manobkantha.com.bd/',
@@ -11,8 +12,10 @@ lis=['https://www.atnbangla.tv/','https://www.dhakatribune.com/','https://www.pr
         'https://www.jamuna.tv/','https://www.ekushey-tv.com/','https://unb.com.bd/',
         'https://www.banglatribune.com/','https://www.jagonews24.com/',
         'https://www.banglanews24.com/','https://www.24livenewspaper.com/',
-        'https://www.priyo.com/','https://banglavision.tv/','https://www.channel24bd.tv/','https://www.ekushey-tv.com/',
-        'https://www.jamuna.tv/','https://maasranga.tv/','https://www.ntvbd.com/','https://www.rtvonline.com/','https://www.somoynews.tv/',
+        'https://www.priyo.com/','https://banglavision.tv/','https://www.channel24bd.tv/',
+        'https://www.ekushey-tv.com/','https://www.jamuna.tv/','https://maasranga.tv/','https://www.ntvbd.com/',
+        'https://www.rtvonline.com/','https://www.somoynews.tv/',
         'https://www.aajkaal.in/','https://www.anandabazar.com/','https://bartamanpatrika.com/',
-        'https://www.bbc.com/bengali/','https://www.dw.com/bn/','https://www3.nhk.or.jp/nhkworld/bn/','https://parstoday.com/bn','https://www.voabangla.com/',
-        'https://www.thedailystar.net/','https://www.newagebd.net/','https://www.daily-sun.com/','https://thebangladeshtoday.com/','https://thefinancialexpress.com.bd/',]
+        'https://www.bbc.com/bengali/','https://www.dw.com/bn/','https://www3.nhk.or.jp/nhkworld/bn/',
+        'https://parstoday.com/bn','https://www.voabangla.com/','https://www.thedailystar.net/','https://www.newagebd.net/',
+        'https://www.daily-sun.com/','https://thebangladeshtoday.com/','https://thefinancialexpress.com.bd/',]
