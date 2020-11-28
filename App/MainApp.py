fcf8e582c75bf558a5e7c1141289486d75cf8fe0
MD. Zariful Huq <52840169+zarif007@users.noreply.github.com>
2020-11-28 16:27:25 +0600

    Update MainApp.py

--- App/MainApp.py
+++ App/MainApp.py
@@ -13,6 +13,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from real_newslist import lis
 import time
 
 
@@ -162,24 +163,7 @@ class NSA(MDApp):
     fc=0
 
     def check_source(self,obj):
-        lis=['https://www.atnbangla.tv/','https://www.dhakatribune.com/','https://www.prothomalo.com/','https://bangla.dhakatribune.com/' ,
-        'https://samakal.com/','https://www.jugantor.com/' 'https://www.dailyinqilab.com/''https://www.dailynayadiganta.com/','https://www.kalerkantho.com/',
-        'https://www.bhorerkagoj.com/','https://www.bangla.24livenewspaper.com/',
-        'https://www.dailyjanakantha.com/','https://www.24livenewspaper.com/',
-        'https://www.bd-journal.com/','https://www.manobkantha.com.bd/',
-        'https://www.dailyjagaran.com/','https://epaper.alokitobangladesh.com/',
-        'https://ebhorerkagoj.com/2020/09/01/','https://epaper.dainikamadershomoy.com/',
-        'https://epaper.manobkantha.com.bd/','https://epaper.ittefaq.com.bd/','https://epaper.shomoyeralo.com/',
-        'https://epaper.manobkantha.com.bd/','https://banglavision.tv/','https://www.channelionline.com/'
-        'https://www.somoynews.tv/','https://www.deshrupantor.com/','https://www.dailyjagaran.com/',
-        'https://www.jamuna.tv/','https://www.ekushey-tv.com/','https://unb.com.bd/',
-        'https://www.banglatribune.com/','https://www.jagonews24.com/',
-        'https://www.banglanews24.com/','https://www.24livenewspaper.com/',
-        'https://www.priyo.com/','https://banglavision.tv/','https://www.channel24bd.tv/','https://www.ekushey-tv.com/',
-        'https://www.jamuna.tv/','https://maasranga.tv/','https://www.ntvbd.com/','https://www.rtvonline.com/','https://www.somoynews.tv/',
-        'https://www.aajkaal.in/','https://www.anandabazar.com/','https://bartamanpatrika.com/',
-        'https://www.bbc.com/bengali/','https://www.dw.com/bn/','https://www3.nhk.or.jp/nhkworld/bn/','https://parstoday.com/bn','https://www.voabangla.com/',
-        'https://www.thedailystar.net/','https://www.newagebd.net/','https://www.daily-sun.com/','https://thebangladeshtoday.com/','https://thefinancialexpress.com.bd/',]
+        
 
     
     
